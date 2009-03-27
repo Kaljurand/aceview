e5d4c6075c2730126c731b1684663654c69ed526
kaljurand <kaljurand@11b4ba50-e0b0-11dd-9059-71fac19e68c6>
2009-03-27 19:58:07 +0000

    * FIXED: Snippet table filtering was based on regexp matching which meant that if you clicked on `work-for' then "John works-for Mary." was not shown in the filtering mode. Now filtering is based on whether the snippet references an entity or not.
    * Removed ManagerViewComponent, was not used/useful.

---
+++ src/ch/uzh/ifi/attempto/aceview/model/filter/PredicateFilter.java
@@ -0,0 +1,94 @@
+package ch.uzh.ifi.attempto.aceview.model.filter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.jdesktop.swingx.decorator.Filter;
+
+import com.google.common.base.Predicate;
+
+public class PredicateFilter extends Filter {
+	private List<Integer> toPrevious;
+	private final Predicate predicate;
+
+
+	/**
+	 * 
+	 * @param predicate
+	 * @param col Column to filter in model coordinates
+	 */
+	public PredicateFilter(Predicate predicate, int col) {
+		super(col);
+		this.predicate = predicate;
+	}
+
+
+	/**
+	 * Resets the internal row mappings from this filter to the previous filter.
+	 */
+	@Override
+	protected void reset() {
+		toPrevious.clear();
+		int inputSize = getInputSize();
+		fromPrevious = new int[inputSize];  // fromPrevious is inherited protected
+		for (int i = 0; i < inputSize; i++) {
+			fromPrevious[i] = -1;
+		}
+	}
+
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	protected void filter() {
+		int inputSize = getInputSize();
+		int current = 0;
+		for (int i = 0; i < inputSize; i++) {
+			if (test(i)) {
+				toPrevious.add(new Integer(i));
+				// generate inverse map entry while we are here
+				fromPrevious[i] = current++;
+			}
+		}
+	}
+
+
+	/**
+	 * <p>Tests whether the given row (in this filter's coordinates) should
+	 * be added.<p>
+	 * 
+	 * @param row the row to test
+	 * @return true iff the row should be added
+	 */
+	private boolean test(int row) {
+		return predicate.apply(getInputValue(row, getColumnIndex()));
+	}
+
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public int getSize() {
+		return toPrevious.size();
+	}
+
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	protected int mapTowardModel(int row) {
+		return toPrevious.get(row);
+	}
+
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	protected void init() {
+		toPrevious = new ArrayList<Integer>();
+	}
+}
