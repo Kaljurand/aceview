ed704cb3d60e46e2164e53c27ac12e1a96188550
kaljurand <kaljurand@11b4ba50-e0b0-11dd-9059-71fac19e68c6>
2009-05-17 19:03:37 +0000

    Renamed:
    ace:Superman -> ace:Universe
    ace:know -> ace:contain

--- src/ch/uzh/ifi/attempto/aceview/util/Showing.java
+++ src/ch/uzh/ifi/attempto/aceview/util/Showing.java
@@ -5,8 +5,12 @@ import java.net.URI;
 import org.semanticweb.owl.model.OWLAxiom;
 import org.semanticweb.owl.model.OWLEntity;
 
-public class Showing {
+public final class Showing {
 
+	private static final URI ENTITY_contain = URI.create("http://attempto.ifi.uzh.ch/ace#contain");
+	private static final URI ENTITY_Universe = URI.create("http://attempto.ifi.uzh.ch/ace#Universe");
+
+	// BUG: deprecated
 	private static final URI ENTITY_know = URI.create("http://attempto.ifi.uzh.ch/ace#know");
 	private static final URI ENTITY_Superman = URI.create("http://attempto.ifi.uzh.ch/ace#Superman");
 
@@ -17,8 +21,11 @@ public class Showing {
 	/**
 	 * <p>Specifies entities which should be displayed as ACE content words.</p>
 	 * 
-	 * <p>Note: we do not show the morph annotations of the following entities:
-	 * owl:Thing, owl:Nothing, ace:Superman, ace:know.</p>
+	 * <p>Note: we do not show the morphological annotations of the following entities:
+	 * <code>owl:Thing</code>,
+	 * <code>owl:Nothing</code>,
+	 * <code>ace:Universe</code>,
+	 * <code>ace:contain</code>.</p>
 	 * 
 	 * TODO: What do to with anonymous individuals?
 	 * 
@@ -27,7 +34,6 @@ public class Showing {
 	 */
 	public static boolean isShow(OWLEntity entity) {
 
-		// isClass && !entity.asOWLClass().isOWLThing() && !entity.asOWLClass().isOWLNothing() ||
 		if (entity.isBuiltIn()) {
 			return false;
 		}
@@ -38,26 +44,33 @@ public class Showing {
 
 		return (
 				entity.isOWLClass() ||
-				entity.isOWLObjectProperty() && !entity.getURI().equals(ENTITY_know) ||
+				entity.isOWLObjectProperty() && !entity.getURI().equals(ENTITY_contain) && !entity.getURI().equals(ENTITY_know) ||
 				entity.isOWLDataProperty() ||
-				entity.isOWLIndividual() && !entity.getURI().equals(ENTITY_Superman)
+				entity.isOWLIndividual() && !entity.getURI().equals(ENTITY_Universe) && !entity.getURI().equals(ENTITY_Superman)
 		);
 	}
 
 
 	/**
 	 * <p>Specifies axioms which should be "shown". For example we do not want to verbalize
-	 * (entailed) axioms which contain "tricks" like using entities `know' and `Superman'.</p>
+	 * (entailed) axioms which contain "tricks" like using entities `contain' and `Universe'
+	 * from the ace-namespace.</p>
 	 * 
 	 * @param axiom OWL axiom
 	 * @return true if axiom should be "shown"
 	 */
 	public static boolean isShow(OWLAxiom axiom) {
 		for (OWLEntity entity : axiom.getReferencedEntities()) {
-			if (entity.getURI().equals(ENTITY_know)) {
+			URI entityURI = entity.getURI();
+			if (entityURI.equals(ENTITY_contain) || entityURI.equals(ENTITY_Universe)) {
 				return false;
 			}
-			if (entity.getURI().equals(ENTITY_Superman)) {
+
+			// BUG: deprecated
+			if (entityURI.equals(ENTITY_know)) {
+				return false;
+			}
+			if (entityURI.equals(ENTITY_Superman)) {
 				return false;
 			}
 		}
