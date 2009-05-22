cb152cd9463dcd16e784c6c50170f428193781d3
kaljurand <kaljurand@11b4ba50-e0b0-11dd-9059-71fac19e68c6>
2009-05-22 20:53:40 +0000

    The tokenizer now supports paragraphs.

--- test/ch/uzh/ifi/attempto/ace/ACETokenizerTest.java
+++ test/ch/uzh/ifi/attempto/ace/ACETokenizerTest.java
@@ -46,4 +46,45 @@ public class ACETokenizerTest {
 						ACEToken.newQuotedString("ŠšŸpŸp")),
 						ACETokenizer.tokenize("123man man123 ŠšŸp 123man123 man123man \"ŠšŸpŸp\""));
 	}
+
+	@Test
+	public final void testTokenize4() {
+		assertEquals(
+				Lists.newArrayList(
+						ACEToken.newToken("John"),
+						ACEToken.newToken("is"),
+						ACEToken.newToken("a"),
+						ACEToken.newToken("man"),
+						ACEToken.DOT,
+						ACEToken.newToken("John"),
+						ACEToken.newToken("likes"),
+						ACEToken.newToken("Mary"),
+						ACEToken.DOT,
+						ACEToken.PARA,
+						ACEToken.newToken("Mary"),
+						ACEToken.newToken("likes"),
+						ACEToken.newToken("John"),
+						ACEToken.DOT
+				),
+				ACETokenizer.tokenize("\n\nJohn is a man.\nJohn likes Mary.\n \n \nMary likes John.\n\n")
+		);
+	}
+
+	@Test
+	public final void testTokenize5() {
+		assertEquals(
+				Lists.newArrayList(
+						ACEToken.newToken("John"),
+						ACEToken.newToken("is"),
+						ACEToken.newToken("a"),
+						ACEToken.newToken("man"),
+						ACEToken.DOT,
+						ACEToken.newToken("John"),
+						ACEToken.newToken("likes"),
+						ACEToken.newToken("Mary"),
+						ACEToken.DOT
+				),
+				ACETokenizer.tokenize("John is a man.#This is a comment.\nJohn likes Mary.\n \n /*\nMary likes John.\n\n\n*/\n")
+		);
+	}
 }
