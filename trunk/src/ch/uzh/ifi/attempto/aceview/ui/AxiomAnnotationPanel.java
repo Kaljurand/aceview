eace47ae465d4e0eb65d8ac8f1bbd16741b5db18
kaljurand <kaljurand@11b4ba50-e0b0-11dd-9059-71fac19e68c6>
2009-06-22 13:35:29 +0000

    Updated AxiomAnnotationPanel to support the new OWLAxiomInstance. (The old version of our AxiomAnnotationPanel crashed with the latest Protege.)

--- trunk/src/ch/uzh/ifi/attempto/aceview/ui/AxiomAnnotationPanel.java
+++ trunk/src/ch/uzh/ifi/attempto/aceview/ui/AxiomAnnotationPanel.java
@@ -7,6 +7,7 @@ import javax.swing.JComponent;
 import javax.swing.JScrollPane;
 
 import org.protege.editor.owl.OWLEditorKit;
+import org.protege.editor.owl.model.util.OWLAxiomInstance;
 import org.protege.editor.owl.ui.frame.OWLAxiomAnnotationsFrame;
 import org.protege.editor.owl.ui.framelist.OWLFrameList2;
 import org.semanticweb.owl.model.OWLAxiom;
@@ -20,22 +21,35 @@ import org.semanticweb.owl.model.OWLAxiom;
 public class AxiomAnnotationPanel extends JComponent {
 
 	private final OWLFrameList2<OWLAxiom> axiomAnnotationComponent;
+	private final OWLAxiomAnnotationsFrame annotationsFrame;
 
 	public AxiomAnnotationPanel(OWLEditorKit eKit) {
 		setLayout(new BorderLayout(6, 6));
 		setPreferredSize(new Dimension(400, 300));
 
-		axiomAnnotationComponent = new OWLFrameList2<OWLAxiom>(eKit, new OWLAxiomAnnotationsFrame(eKit));
+		annotationsFrame = new OWLAxiomAnnotationsFrame(eKit);
+		axiomAnnotationComponent = new OWLFrameList2<OWLAxiom>(eKit, annotationsFrame);
 		axiomAnnotationComponent.setAxiomSelectionSyncronized(false);
 
 		add(new JScrollPane(axiomAnnotationComponent));
-
 		setVisible(true);
 	}
 
 
-	public void setAxiom(OWLAxiom axiom) {
-		axiomAnnotationComponent.setRootObject(axiom);
+	/**
+	 * <p>Annotations apply to a particular instance of an axiom, i.e.
+	 * the axiom must be qualified by its containing ontology.</p>
+	 * 
+	 * @param axiomInstance Instance of the axiom
+	 */
+	public void setAxiom(OWLAxiomInstance axiomInstance) {
+		if (axiomInstance != null) {
+			axiomAnnotationComponent.setRootObject(axiomInstance.getAxiom());
+			annotationsFrame.setContainingOntology(axiomInstance.getOntology());
+		}
+		else{
+			axiomAnnotationComponent.setRootObject(null);
+		}
 	}
 
 
