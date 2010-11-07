3534535b966acf1e30731701af35fdc5e1026a50
kaljurand <kaljurand@11b4ba50-e0b0-11dd-9059-71fac19e68c6>
2010-11-07 22:43:54 +0000

    fixed: ACESnippetImpl: equals and hashCode did not consider the snippets where the axioms were not verbalized added: doc: Aneurist example

--- src/ch/uzh/ifi/attempto/aceview/ACETextManager.java
+++ src/ch/uzh/ifi/attempto/aceview/ACETextManager.java
@@ -346,11 +346,10 @@ public final class ACETextManager {
 	 * @return OWL ontology manager
 	 */
 	public static OWLOntologyManager createOWLOntologyManager() {
-		return OWLManager.createOWLOntologyManager();
-		//if (owlModelManager == null) {
-		//	OWLManager.createOWLOntologyManager();
-		//}
-		//return OWLManager.createOWLOntologyManager(owlModelManager.getOWLDataFactory());
+		if (owlModelManager == null) {
+			return OWLManager.createOWLOntologyManager();
+		}
+		return OWLManager.createOWLOntologyManager(owlModelManager.getOWLDataFactory());
 	}
 
 
